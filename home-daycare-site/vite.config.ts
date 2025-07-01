f0a4dd7774f8b608425196aaea4192a245def2ea
DaBurgermeista <63371712+DaBurgermeista@users.noreply.github.com>
2025-07-01 13:31:53 +0000

    Updates

--- home-daycare-site/vite.config.ts
+++ home-daycare-site/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "https://github.com/DaBurgermeista/home-daycare-site",
+  base: "/home-daycare-site/",
   plugins: [tailwindcss()],
 });
